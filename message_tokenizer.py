886bf4e0bb6be3685799c16f65ef2d2ab0888cc6
Keith <keithbradley1@msn.com>
2016-07-19 14:49:47 -0500

    created initial testing for the test_behavior_predictor.py

--- message_tokenizer.py
+++ message_tokenizer.py
@@ -1,6 +1,65 @@
-class tokenizer():
-  class alphanumeric_characters():
-  class word_count(): #start here
-  class word_position():
-  class sentence_count():
-  class punctuation():
+import unittest
+
+class TestMessageTokenizer(unittest.TestCase):
+''' Receives a twitter message as a string to be tokenized below'''
+
+
+@classmethod
+  def setUpClass(self):
+    ''' Our doc string '''
+    pass
+
+  def test_(self):
+    ''' Our doc string '''
+    pass
+
+# -----------------Alphanumeric Character Set Functions----------------------
+  def test_that_message_passed_in_is_a_string(self):
+    ''' Checks to make sure that nothing besides a string is passed in '''
+    #assertIs
+    pass
+
+  def test_all_characters_are_alphanumeric(self):
+    ''' Checks that that characters are alphanumeric characters '''
+    pass
+
+  def test_alphanumeric_characters_are_a_set(self):
+    ''' Checks that alphanumeric characters are a set'''
+    pass
+
+# -----------------Word Count----------------------
+  def test_message_is_a_list(self):
+    ''' Checks that message is a list'''
+    pass
+
+  def test_punctuation_is_removed(self):
+    ''' Checks that there is no punctuation'''
+    pass
+
+  def test_that_word_count_returns_integer(self):
+    ''' Checks that word count returns an integer'''
+    pass
+
+# -----------------Word Position----------------------
+  def test_message_is_a_list(self):
+    ''' Checks that message is a list'''
+    pass
+
+  def test_message_list_length_is_integer(self):
+    ''' Checks that list length is an integer'''
+    pass
+
+  def test_message_list_length_equals_index_list_plus_one(self):
+    ''' Checks that list length equals index list plus one'''
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+# Alphanumeric Characters
+# Word count
+# Word position
+# Sentence count
+# Punctuation
