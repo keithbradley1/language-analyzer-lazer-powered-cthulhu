e0088f7d66d1c1bd8232418a4cc549ef8f3bcb09
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-07-19 13:52:19 -0500

    draft TestMessageTokenizer.py

--- test_message_tokenizer.py
+++ test_message_tokenizer.py
@@ -1,7 +1,7 @@
 import unittest
 
-class Cthulhu(unittest.TestCase):
-''' Our doc string '''
+class TestMessageTokenizer(unittest.TestCase):
+''' Receives a twitter message as a string to be tokenized below'''
 
 
 @classmethod
@@ -13,5 +13,65 @@ class Cthulhu(unittest.TestCase):
     ''' Our doc string '''
     pass
 
-if __name__ == '__main__':
-    unittest.main()
+# -----------------Alphanumeric Character Set Functions----------------------
+  def test_that_message_passed_in_is_a_string(self):
+    ''' Checks to make sure that nothing besides a string is passed in '''
+    #assertIs
+    pass
+
+  def test_all_characters_are_alphanumeric(self):
+    ''' Checks that that characters are alphanumeric characters '''
+    pass
+
+  def test_alphanumeric_characters_are_a_set(self):
+    ''' Checks that alphanumeric characters are a set'''
+    pass
+
+# -----------------Word Count----------------------
+  def test_message_is_a_list(self):
+    ''' Checks that message is a list'''
+    pass
+
+  def test_punctuation_is_removed(self):
+    ''' Checks that there is no punctuation'''
+    pass
+
+  def test_that_word_count_returns_integer(self):
+    ''' Checks that word count returns an integer'''
+    pass
+
+# -----------------Word Position----------------------
+  def test_message_is_a_list(self):
+    ''' Checks that message is a list'''
+    pass
+
+  def test_message_order_is_unchanged(self):
+    pass
+
+  def test_message_list_length_is_integer(self):
+    ''' Checks that list length is an integer'''
+    pass
+
+  def test_message_list_length_equals_index_list_plus_one(self):
+    ''' Checks that list length equals index list plus one'''
+    pass
+
+# -----------------Sentence Count----------------------
+
+  def test_message_contains_punctuation(self):
+    #test for period, question, exclamation point
+    pass
+
+  def test_message_list_contains_only_strings(self):
+    pass
+
+  def test_each_message_string_contains_punctuation(self):
+    pass
+
+
+
+# Alphanumeric Characters
+# Word count
+# Word position
+# Sentence count
+# Punctuation
