9c9df24c7d211ede4d1dabad4bb6c37242fa7c9d
Jessica Wynn <Jessicashinjo@gmail.com>
2016-07-20 13:46:44 -0500

    Initial draft of Tokenizer tests written

--- message_tokenizer.py
+++ message_tokenizer.py
@@ -1,6 +1,17 @@
-class tokenizer():
-  class alphanumeric_characters():
-  class word_count(): #start here
-  class word_position():
-  class sentence_count():
-  class punctuation():
+class Tokenizer():
+  pass
+#   self.message
+#
+#   split_message()
+#   '''splits on spaces'''
+#   alphanumeric():
+#   word_count(): #start here
+#   word_position():
+#   message_length()
+#   sentence_count
+#     split sentence on punctuation
+#   punctuation
+#   no_punctuation
+#   lowercase
+
+# "This is my message!"
