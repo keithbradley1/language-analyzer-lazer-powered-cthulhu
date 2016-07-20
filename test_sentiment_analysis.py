d5e019fb356c683e4de6e3119a3a4c69d819fe9a
Amari Ashwood <fromashestosnow@gmail.com>
2016-07-20 09:22:19 -0500

    added first try to SentimentTests

--- test_sentiment_analysis.py
+++ test_sentiment_analysis.py
@@ -1,4 +1,95 @@
+import unittest
+from language_analyzer import *
+
+########################## Sentiment Analysis Test ################################
+
 class sentiment_analysis(a):
-  class positive():
-  class negative():
-  class neutral():
+	'''get key value pairs from the lexicon
+	each value should have True or False
+	giving it a value'''
+
+######################## Test for Positive #######################################
+
+
+class TestPositive(unittest.TestCase):
+	self.assertIs(dict)
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+	def test_for_reciept_of_dict(self):
+		'''Test that the list has keywords for positive'''
+	self.assertIs(dict)
+
+	def test_for_matches(self):
+		'''Test that keywords match'''
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+		self.assertEqual(positive)
+
+	def test_that_it_stores_matches_in_dict(self):
+		'''Tests that matches are stored in a dict'''
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+		self.assertIs(dict)
+
+######################## Test for Negative #######################################
+
+
+class TestNegative():
+	self.assertIs(dict)
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+	def test_for_reciept_of_dict(self):
+		'''Test that the list has keywords for negative'''
+	self.assertIs(dict)
+
+	def test_for_matches(self):
+		'''Test that keywords match'''
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+		self.assertEqual(negative)
+
+	def test_that_it_stores_matches_in_dict(self):
+		'''Tests that matches are stored in a dict'''
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+		self.assertIs(dict)
+
+######################## Test for Neutral #######################################
+
+class TestNeutral():
+  	self.assertIs(dict)
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+	def test_for_reciept_of_dict(self):
+	'''Test that the list has keywords for neutral'''
+	self.assertIs(dict)
+
+	def test_for_matches(self):
+	'''Test that keywords match'''
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+		self.assertEqual(neutral)
+
+	def test_that_it_stores_matches_in_dict(self):
+	'''Tests that matches are stored in a dict'''
+	message = self.message
+	for x in message:
+		assertEqual(True, isinstance(x, message))
+
+		self.assertIs(dict)
