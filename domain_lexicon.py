d83ba8d2a0e50ed884cdff25395f1990863361d6
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-07-20 15:45:18 -0500

    passed initial tests

--- domain_lexicon.py
+++ domain_lexicon.py
@@ -1,6 +1,5 @@
-domain_lexicon =
-    {
+domain_lexicon = {
       "healthcare": [('obamacare', .08), ('hospital', .08), ('insurance', .08), ('healthcare', .08)],
       "war": [('enemies', .08), ('war', .08)],
       "climate change": [('climate', .08), ('greenhouse', .08), ('aerosols', .08), ('deforestation', .08), ('atmosphere', .08)]
-    }
+      }
