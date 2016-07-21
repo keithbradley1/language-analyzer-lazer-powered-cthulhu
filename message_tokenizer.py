a67c9d5d300661b7af9aa918df1e8bc7b4ceece8
Jessica Wynn <Jessicashinjo@gmail.com>
2016-07-21 17:42:46 -0500

    All tests are passing and all functions contain doc strings

--- message_tokenizer.py
+++ message_tokenizer.py
@@ -1,13 +1,15 @@
 import string
+import re
 
 
 class Tokenizer():
+  '''  '''
   def __init__(self, message):
     self.message = message
-    pass
 
   def lowercase(self):
-
+    ''' Takes a string, makes all characters lowercased, and then returns
+    the lowercased string '''
     lowercase_message = self.message.lower()
     return lowercase_message
 
@@ -17,48 +19,51 @@ class Tokenizer():
     of strings and adds each alphanumeric character to a set'''
     alphanumeric_set = set()
     lowercase_character_list = list(self.message)
+
     for toke in lowercase_character_list:
       if toke.isalnum():
         alphanumeric_set.add(toke)
-
     return alphanumeric_set
 
   def split_message(self):
-    # message_without_spaces = list()
+    ''' Splits the sentence up into words on spaces '''
     split_message = self.message.split(" ")
-    # for word in split_message:
-    #   message_without_spaces.extend(word)
-
     return split_message
 
-
-
-  # def no_punctuation(self):
-  #   ''' Takes the message as a string and returns the stringed messaged
-  #   without punctuation '''
-
   def stripped_message(self):
+    ''' Takes the message, runs the split_message function, and then
+    removes all punctuation '''
     split_sentence = self.split_message()
     plain_message_list = list()
 
     for word in split_sentence:
       plain_message_list.append(word.rstrip(string.punctuation))
-
     return plain_message_list
 
+  def word_count(self):
+    ''' Takes the stripped_message and counts the length of the list '''
+    message = self.stripped_message()
+    word_count = len(message)
+    return word_count
 
-# fish = Tokenizer("This is my message!")
-# gum = fish.stripped_message()
-#
-# print(gum)
-#   '''splits on spaces'''
-#
-#   word_position():
-#   message_length()
-#   sentence_count
-#     split sentence on punctuation
-#   punctuation
-#
-#   lowercase
+  def word_position(self):
+    ''' Returns each word in the message with its index in the message '''
+    message = self.stripped_message()
+    word_position = list(enumerate(message))
+    return word_position
 
-# "This is my message!"
+  def sentence_count(self):
+    ''' Splits the message on period, exclamation point, and question mark.
+    Then gives the sentence count as an integer '''
+    split_message = re.split("[.!?]+", self.message)
+    return len(split_message)
+
+  def punctuation(self):
+    ''' Returns a list of all punctuation contained in the message '''
+    punctuation_list = list()
+
+    for character in self.message:
+      if character in string.punctuation:
+        punctuation_list.append(character)
+
+    return punctuation_list
